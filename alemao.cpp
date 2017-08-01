acc611029980ec313d2c7a8ebc2bad5fd87e0cc2
Lucas V C Nicolau <lcsvcn@users.noreply.github.com>
2017-08-01 19:03:48 -0300

    adicionado exemplo de dicionario.txt e adicionado genero

--- alemao.cpp
+++ alemao.cpp
@@ -10,7 +10,7 @@ using namespace std;
 /*
 typedef struct dicionario {
 	string chave;
-	string artigo_alemao;
+	string genero_alemao;
 	string portugues;
 	string ingles;
 } DIC;
@@ -27,7 +27,7 @@ int main() {
 	DIC d;
 	if(!feof(arq)) {
 		fread(&d, 1, sizeof(DIC), arq);
-		printf("----------------------------\nch:%s\nart:%s\npt:%s\nen:%s\n----------------------------", d.chave, d.artigo_alemao, d.portugues, d.ingles);		
+		printf("----------------------------\nch:%s\nart:%s\npt:%s\nen:%s\n----------------------------", d.chave, d.genero_alemao, d.portugues, d.ingles);		
 	}
 
 	fclose(arq); 		// fecha arquivo
@@ -44,33 +44,47 @@ int main() {
 #include <string>
 #include <sstream>
 #include <map>
-#include <tchar.h>
+
+#define PORTUGUES 1
+#define INGLES 2
+#define ALEMAO 3
 
 using namespace std;
 
 typedef struct dicionario {
-	string artigo_alemao;
+	string genero_alemao;
+	string genero_portugues;
 	string portugues;
 	string ingles;
+	string alemao;
 } DIC;
 
 int main() {	
 	ifstream file;
 	string filename = "dicionario.txt";	// nome do arquivo
 	file.open(filename.c_str());    // abre o arquivo
-
 	int index = 0;			// index do str_chave ou str_buscada
-	
+	int op;
+
 	string str_leitura;		// str lida
 	string token;			// str lida quebrada em duas
-	string str_chave;		// str_chave do map
+	string lixo;		
 	string str_buscada;		// str_buscada do map
 
 
 	DIC d;
 
-
 	std::map<string, DIC> mapa;
+	
+	printf("-------------------\n");
+	printf("Idioma de busca\n");
+	printf("-------------------\n");
+	printf("1 - Portugues\n");
+	printf("2 - Ingles\n");
+	printf("3 - Alemao\n");
+	printf("-------------------\n");
+
+	cin >> op;
 	
 	// le dicionario
 	while(getline(file, str_leitura)) {
@@ -82,38 +96,62 @@ int main() {
 
 		stringstream sstr(str_leitura);
 		
-		while(getline(sstr, token, '#')) {		
+		while(getline(sstr, token, '#')) {	
 			if(index == 0) {		// index == 0 --> str_chave
-				str_chave = token;
+				d.genero_portugues = token; 
 				index++;
-			} else if(index == 1) {				// index == 0 --> str_buscada
-				d.artigo_alemao = token;
+			} else if(index == 1) {
+				d.portugues = token;
 				index++;
-			} else if(index == 2) {
+			} else if(index == 2) {				// index == 0 --> str_buscada
+				d.genero_alemao = token;
+				index++;
+			} else if(index == 3) {
+				d.alemao = token;
+				index++;
+			} else if(index == 4) {
 				d.ingles = token;
-				index++;
-			} else {
-				d.portugues = token; 
-				mapa.insert(pair<string, DIC>(str_chave, d));
-				index = 0;			// zera o index
+				if(op == PORTUGUES) {
+					mapa.insert(pair<string, DIC>(d.portugues, d));
+				} else if(op == INGLES) {
+						mapa.insert(pair<string, DIC>(d.ingles, d));
+				} else if(op == ALEMAO) {
+						mapa.insert(pair<string, DIC>(d.alemao, d));		
+				}
+ 				index = 0;			// zera o index
 			}
 		}
 	}
 
 		
 	map<string, DIC>::iterator it;
+
+	getline(cin, lixo);
 	
+	printf("-------------------\n");
+	printf("Buscar: ");
 	while(getline(cin, str_leitura)) {
+
+	printf("-------------------\n");
 		it = mapa.find(str_leitura);
 		
 		if(it != mapa.end()) {
-			cout << it->second.artigo_alemao << endl;				// achou imprime resultado
-			cout << it->second.ingles << endl;				// achou imprime resultado
-			cout << it->second.portugues << endl;				// achou imprime resultado
+			if(op != PORTUGUES) {
+				cout << "(" << it->second.genero_portugues << ") ";				// achou imprime resultado
+				cout << it->second.portugues << endl;				// achou imprime resultado
+			}
+			if(op != ALEMAO) {
+				cout << "(" << it->second.genero_alemao << ") ";				// achou imprime resultado
+				cout << it->second.alemao << endl;				// achou imprime resultado
+			}
+			if(op != INGLES) {
+				cout << it->second.ingles << endl;				// achou imprime resultado
+			}
 		} else {
 			cout << "nao encontrado" << endl;		// nao achou precisa adicionar no dicionario
 		}
 
+		printf("-------------------\n");
 	}
 
 
